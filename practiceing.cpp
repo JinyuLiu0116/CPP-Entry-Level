ae95511806c4dec1d56d3e1a299c7e7659fc4a3f
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-10-22 22:20:49 -0400

    Create practiceing.cpp

---
+++ practiceing.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+  int userInputTemp;
+  const int TENSENO = -99;
+  int counter = 0;
+  std::cout <<"Then senseio temperature is:" << TENSENO << std::endl;
+  std::cout <<"Please enter the temperatures entill meet the tenseio number" << std::endl;
+  while(true){
+    int inputNum;
+    std::cout <<"Enter your temperature number:";
+    std::cin >> inputNum;
+  }
+}
