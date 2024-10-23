a5ac07eeb0bbd2a881f91047ce9368a49331a9cf
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-10-22 22:26:37 -0400

    Update practiceing.cpp

--- practiceing.cpp
+++ practiceing.cpp
@@ -4,15 +4,25 @@ int main(){
   int userInputTemp;
   const int TENSENO = -99;
   int counter = 0;
+  int sumOfTemp = 0;
   std::cout <<"Then senseio temperature is:" << TENSENO << std::endl;
   std::cout <<"Please enter the temperatures entill meet the tenseio number" << std::endl;
   while(true){
     int inputNum;
     std::cout <<"Enter your temperature number:";
     std::cin >> inputNum;
-    if(inputNum < TENSENO || inputNum > 100){
+    if(inputNum == null){
+      std::cout <<"Empty input" << std::endl;
+      continue;
+    }
+    else if(inputNum < TENSENO || inputNum > 100){
       std::cout <<"Temperature: " << inputNum << " out of range" << std::endl;
-      contune;
+      continue;
+    }
+    else{
+      sumOfTemp += inputNum;
+      counter++;
+      break;
     }
     
   }
